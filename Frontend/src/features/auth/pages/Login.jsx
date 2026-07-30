6511030b3b16728eab0ef63f8d74e4be0597ea4d
Sarit <saritbose26@gmail.com>
2026-07-30 18:18:33 +0530

    Login & Register Page UI Completed

---
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { useNavigate, Link } from "react-router";
+import "../auth.form.scss";
+
+const Login = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <main>
+      <div className="form-container">
+        <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Enter email addess"
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Enter password"
+            />
+          </div>
+
+          <button className="button primary-button">Login</button>
+        </form>
+
+        <p>
+          Don't have an account? <Link to={"/register"}>Register</Link>
+        </p>
+      </div>
+    </main>
+  );
+};
+
+export default Login;
---
+++ Frontend/src/features/auth/pages/Register.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { useNavigate, Link } from "react-router";
+
+const Register = () => {
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <main>
+      <div className="form-container">
+        <h1>Register</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <label htmlFor="email">Username</label>
+            <input
+              type="text"
+              name="username"
+              id="username"
+              placeholder="Enter username"
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Enter email addess"
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Enter password"
+            />
+          </div>
+
+          <button className="button primary-button">Register</button>
+        </form>
+
+        <p>
+          Already have an account? <Link to={"/login"}>Login</Link>
+        </p>
+      </div>
+    </main>
+  );
+};
+
+export default Register;
