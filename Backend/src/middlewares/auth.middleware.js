3c7c818b12bf7c9d5af1ce735e11fbf2489e593c
Sarit <saritbose26@gmail.com>
2026-07-30 17:35:33 +0530

    Frontend SetUp Completed

--- Backend/src/middlewares/auth.middleware.js
+++ Backend/src/middlewares/auth.middleware.js
@@ -1,12 +1,21 @@
 const jwt = require("jsonwebtoken");
+const tokenBlacklistModel = require("../models/blacklist.model");
 
-function authUser(req, res, next) {
+async function authUser(req, res, next) {
   const token = req.cookies.token;
   if (!token) {
     return res.status(401).json({
       message: "Token not provided.",
     });
   }
+
+  const isTokenBlacklisted = await tokenBlacklistModel.findOne({ token });
+  if (isTokenBlacklisted) {
+    return res.status(401).json({
+      message: "Token is Invalid.",
+    });
+  }
+
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
